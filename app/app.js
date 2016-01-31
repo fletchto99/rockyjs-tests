5f934bbd1d828b271793c5512541fd87493d0b67
Matt Langlois <fletchto99@gmail.com>
2016-01-31 16:34:38 -0500

    Working algorithm -- can be improved though

--- app/app.js
+++ app/app.js
@@ -1,34 +1,132 @@
 function app() {
-    console.log("hi");
-    console.log(document.getElementById("pebble"));
-    var rocky = Rocky.bindCanvas(document.getElementById("pebble"));
-    rocky.export_global_c_symbols();
+    'use strict';
 
-    var bmp = gbitmap_create(
-        'https://github.com/apple-touch-icon-57x57.png'
-    );
+    var seed = location.hash.substr(1) || 'random';
 
-    rocky.update_proc = function (ctx, bounds) {
 
-        var arc = function(angle_ratio, inset, color, thickness) {
-            var rect = grect_inset(bounds, inset);
-            graphics_context_set_fill_color(ctx, color);
-            graphics_fill_radial(
-                ctx, rect, GOvalScaleModeFitCircle,
-                thickness || 12, // thickness of each arc
-                DEG_TO_TRIGANGLE(0),
-                DEG_TO_TRIGANGLE(360 * angle_ratio)
-            );
-        };
-        graphics_draw_bitmap_in_rect(ctx, bmp, [43, 55, 57, 57]);
+    var seeds = {
+        glider: 'https://fletchto99.com/other/pebble/rocky/app/boards/glider.json'
+    };
+    var interval = null;
+    var board = [];
 
-        var d = new Date();
-        arc((d.getHours() % 12) / 12, 30, GColorJazzberryJam);
-        arc(d.getMinutes() / 60, 15, GColorJaegerGreen);
-        arc(d.getSeconds() / 60, 0, GColorBlueMoon);
-        console.log(bmp);
+    window.addEventListener("hashchange", function () {
+        seed = location.hash.substr(1);
+        if (interval != null) {
+            clearInterval(interval);
+        }
+        go();
+    }, false);
+
+    var go = function() {
+        board = [];
+        if (seed == 'random') {
+            for (var i = 0; i < 48; i++) {
+                var row = [];
+                for (var j = 0; j < 48; j++) {
+                    row.push(Math.floor(Math.random() * 2));
+                }
+                board.push(row);
+            }
+            interval = setInterval(function () {
+                rocky.mark_dirty();
+            }, 100);
+        } else {
+            if (seeds[seed] === undefined) {
+                console.error('Invalid seed!');
+                return;
+            }
+
+            var request = new XMLHttpRequest();
+            request.onreadystatechange = function () {
+                if (request.readyState == 4 && request.status == 200) {
+                    var response = JSON.parse(request.responseText);
+                    var fill = response.Mode == 'enable';
+                    for (var i = 0; i < 48; i++) {
+                        var row = [];
+                        for (var j = 0; j < 48; j++) {
+                            var found = false;
+                            response.Points.forEach(function (point) {
+                                if (!found && point.y == i && point.x == j) {
+                                    row.push(fill ? 1 : 0);
+                                    found = true;
+                                }
+                            });
+                            if (!found) {
+                                row.push(fill ? 0 : 1);
+                            }
+                        }
+                        board.push(row);
+                    }
+                    interval = setInterval(function () {
+                        rocky.mark_dirty();
+                    }, 100);
+                }
+            };
+            request.open("GET", seeds[seed], true);
+            request.send();
+        }
+    };
+
+
+
+    /**
+     * Some really inefficient way of calculating all of the neighbours
+     */
+    var countNeighbours = function(x,y,board) {
+        var neighbours = [];
+
+        if (board[y - 1]) {
+            neighbours.push(board[y - 1][x - 1]);
+            neighbours.push(board[y - 1][x]);
+            neighbours.push(board[y - 1][x + 1]);
+        }
+
+        neighbours.push(board[y][x - 1]);
+        neighbours.push(board[y][x + 1]);
+
+        if (board[y + 1]) {
+            neighbours.push(board[y + 1][x - 1]);
+            neighbours.push(board[y + 1][x]);
+            neighbours.push(board[y + 1][x + 1]);
+        }
+
+        var filtered = neighbours.filter(function(neighbour) {
+            return neighbour !== undefined && neighbour == 1;
+        });
+        return filtered.length;
 
     };
 
-    setInterval(function () {rocky.mark_dirty();}, 10);
+    var rocky = Rocky.bindCanvas(document.getElementById("pebble"));
+    rocky.export_global_c_symbols();
+    rocky.update_proc = function (ctx) {
+
+        var nextGenBoard = board.map(function (arr) {
+            return arr.slice();
+        });
+
+        for(var row = 0; row < board.length; row++) {
+            for(var column = 0; column < board[row].length; column++) {
+                board[row][column] == 1 ? graphics_context_set_fill_color(ctx, GColorBlack): graphics_context_set_fill_color(ctx, GColorWhite);
+                graphics_fill_rect(ctx, GRect(column*3, row*3, 3, 3));
+                var neighbourCount = countNeighbours(column, row, board);
+                if (neighbourCount < 2 && board[row][column] == 1) {
+                    nextGenBoard[row][column] = 0;
+                } else if (neighbourCount > 3 && board[row][column] == 1) {
+                    nextGenBoard[row][column] = 0;
+                } else if (neighbourCount == 3 && board[row][column] == 0) {
+                    nextGenBoard[row][column] = 1;
+                }
+            }
+        }
+
+        board = nextGenBoard;
+
+        graphics_context_set_fill_color(ctx, GColorBlack);
+        graphics_draw_line(ctx, GPoint(0, 144), GPoint(144, 144));
+
+    };
+
+    go();
 }
